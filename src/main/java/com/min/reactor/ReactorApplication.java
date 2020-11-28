133d459176ba1ac7820d13697276464c309c221f
Min1579 <hsm012362@gmail.com>
2020-11-29 01:33:21 +0900

    feat ⚡️ reactor study project init

---
+++ src/main/java/com/min/reactor/ReactorApplication.java
@@ -0,0 +1,12 @@
+package com.min.reactor;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ReactorApplication {
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
