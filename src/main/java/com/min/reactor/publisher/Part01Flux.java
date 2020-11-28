736d70a5adacf6891b4e214ee8e4f150bd338829
Min1579 <hsm012362@gmail.com>
2020-11-29 01:29:17 +0900

    flux 🤙 reactor-study-day1

---
+++ src/main/java/com/min/reactor/publisher/Part01Flux.java
@@ -0,0 +1,36 @@
+package com.min.reactor.publisher;
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Part01Flux {
+
+    // TODO Return an empty Flux
+    Flux<String> emptyFlux() {
+        return Flux.empty();
+    }
+
+    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+    Flux<String> fooBarFluxFromValues() {
+        return Flux.just("Foo", "bar");
+        //return Flux.just(new String[]{"foo","bar"});
+    }
+
+    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+    Flux<String> fooBarFluxFromList() {
+        return Flux.fromIterable(List.of("foo","bar"));
+        //return Flux.fromIterable(Arrays.asList("foo","bar"));
+    }
+
+    // TODO Create a Flux that emits an IllegalStateException
+    Flux<String> errorFlux() {
+        return Flux.error(new IllegalStateException());
+    }
+
+    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+    Flux<Long> counter() {
+        return Flux.interval(Duration.ofMillis(100)).take(10);
+    }
+}
